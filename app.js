cf8f62382214d99bb71f44c85ce58e46337f4a26
rahuljain1310 <ee1170476@iitd.ac.in>
2019-07-05 09:22:14 +0530

    Form Feature Added

--- app.js
+++ app.js
@@ -1,34 +1,66 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+let express = require('express') 
+let bodyParser = require('body-parser')
+let nodemailer = require('nodemailer')
+let app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 const CONTACT_ADDRESS = 'rahuljain13101999@gmail.com';
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
-
-var mailer = require('nodemailer').createTransport({
-  service: 'Gmail',
+var transport = {
+  host: 'smtp.gmail.com',
   auth: {
     user: 'rahul.website.worker@gmail.com',
-    pass: 'elex1310',
+    pass: 'HE3kkSzv'
   }
+}
+
+var transporter = nodemailer.createTransport(transport)
+transporter.verify((error, success) => {
+  if (error) console.log(error);
+  else console.log('Server is ready to take messages');
 });
 
-server.post('/contact', function(req, res) {
-  mailer.sendMail({
-    from: req.body.from,
-    to: [CONTACT_ADDRESS],
-    subject: req.body.subject || '[No subject]',
-    html: req.body.message || '[No message]',
-  }, function(err, info) {
-    if (err) return res.status(500).send(err);
-    res.json({success: true});
+app.post('/contact', (req, res, next) => {
+  var from = req.body.from || 'Anonymous'
+  var email = req.body.email || 'No Email Provided'
+  var message = req.body.message
+  var subject = 'Contact Website: '+ ( req.body.subject || '[No subject]' )
+  var content = `name: ${from} \n email: ${email} \n message: ${message} `
+
+  var mail = {
+    from: from,
+    to: [CONTACT_ADDRESS],  //Change to email address that you want to receive messages on
+    subject: subject,
+    text: content
+  }
+
+  console.log(mail)
+
+  transporter.sendMail(mail, (err, data) => {
+    console.log('Mail Response')
+    if (err)  {
+      console.log(err)
+      res.status(404).json({'msg': 'fail'})
+    }
+    else {
+      console.log('successful mail send')
+      res.status(200).json({'msg': 'success'})
+    }
   })
-});
+})
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
+app.get('/', (req,res) => {
+  res.send('Hello World')
+})
+
+app.listen(3000, () => {
+  console.log('Server running at port 3000')
 });
---
+++ js/form.js
@@ -0,0 +1,25 @@
+handleSubmit = () => {
+  var form = document.getElementById('contactformInputs').elements;
+  data = {
+    from: form['from'].value,
+    email: form['email'].value,
+    message: form['message'].value,
+    subject: form['subject'].value,
+  }
+  console.log(data)
+  fetch("http://localhost:3000/contact", {
+    method: "POST",
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  }).then((response)=>{
+      console.log(response)
+      if (response.status === 200){
+          alert("Message Sent."); 
+          this.resetForm()
+      }else if(response.status === 404){
+          alert("Message failed to send.")
+      }
+  })
+}
